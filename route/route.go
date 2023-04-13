92244e0a430e60dd4d91fcd734f2cc7627909391
rikurunico <rikurunico@gmail.com>
2023-04-13 14:35:26 +0700

    Refactor router setup to use group for quotes endpoints.

--- route/route.go
+++ route/route.go
@@ -7,9 +7,13 @@ import (
 
 func SetupRoute(router *gin.Engine) {
 
-	router.GET("/quotes", handler.GetQuotes)
-	router.GET("/quotes/:id", handler.GetQuoteByID)
-	router.POST("/quotes", handler.CreateQuote)
-	router.PUT("/quotes/:id", handler.UpdateQuote)
-	router.DELETE("/quotes/:id", handler.DeleteQuote)
+	quotes := router.Group("/quotes")
+	{
+		quotes.GET("", handler.GetQuotes)
+		quotes.GET("/:id", handler.GetQuoteByID)
+		quotes.POST("", handler.CreateQuote)
+		quotes.PUT("/:id", handler.UpdateQuote)
+		quotes.DELETE("/:id", handler.DeleteQuote)
+	}
+
 }
