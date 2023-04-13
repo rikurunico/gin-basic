a5fdf3bec9fb9df02db66367c5ab8cc8faf4886f
rikurunico <rikurunico@gmail.com>
2023-04-13 14:27:37 +0700

    Refactor handler package for best practice and clean code

--- handler/controller.go
+++ handler/controller.go
@@ -1,73 +1,73 @@
 package handler
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/rikurunico/gin-basic/model"
-	"github.com/rikurunico/gin-basic/database"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+	"github.com/rikurunico/gin-basic/database"
+	"github.com/rikurunico/gin-basic/model"
 )
 
 func GetQuotes(c *gin.Context) {
 	var quotes []model.Quote
-	database.DB.Find(&quotes)
-
-	c.JSON(200, quotes)
+	if err := database.DB.Find(&quotes).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, quotes)
 }
 
 func GetQuoteByID(c *gin.Context) {
 	var quote model.Quote
 	id := c.Param("id")
-	database.DB.First(&quote, id)
-
-	if quote.ID == 0 {
-		c.JSON(404, gin.H{"message": "quote not found"})
+	if err := database.DB.First(&quote, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"message": "quote not found"})
 		return
 	}
-
-	c.JSON(200, quote)
+	c.JSON(http.StatusOK, quote)
 }
 
 func CreateQuote(c *gin.Context) {
 	var quote model.Quote
 	if err := c.ShouldBindJSON(&quote); err != nil {
-		c.JSON(400, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-
-	database.DB.Create(&quote)
-
-	c.JSON(201, quote)
+	if err := database.DB.Create(&quote).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, quote)
 }
 
 func UpdateQuote(c *gin.Context) {
 	var quote model.Quote
 	id := c.Param("id")
-
 	if err := database.DB.First(&quote, id).Error; err != nil {
-		c.JSON(404, gin.H{"message": "quote not found"})
+		c.JSON(http.StatusNotFound, gin.H{"message": "quote not found"})
 		return
 	}
-
 	if err := c.ShouldBindJSON(&quote); err != nil {
-		c.JSON(400, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-
-	database.DB.Save(&quote)
-
-	c.JSON(200, quote)
+	if err := database.DB.Save(&quote).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, quote)
 }
 
 func DeleteQuote(c *gin.Context) {
 	var quote model.Quote
 	id := c.Param("id")
-
 	if err := database.DB.First(&quote, id).Error; err != nil {
-		c.JSON(404, gin.H{"message": "quote not found"})
+		c.JSON(http.StatusNotFound, gin.H{"message": "quote not found"})
 		return
 	}
-
-	database.DB.Delete(&quote)
-
-	c.JSON(200, gin.H{"message": "quote deleted"})
+	if err := database.DB.Delete(&quote).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "quote deleted"})
 }
